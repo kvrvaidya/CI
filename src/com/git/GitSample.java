faa3e2d3fe9a72c4eb31f34a1f71d12b7e83dc4b
prasanth <sunnyvaidya8@gmail.com>
2016-12-26 13:55:24 +0530

    commited

--- src/com/git/GitSample.java
+++ src/com/git/GitSample.java
@@ -2,11 +2,12 @@ package com.git;
 
 public class GitSample {
 	
+	public static void main(String[]args){
+		System.out.println(add(3,5));
+	}
 	public static int add(int a ,int b){
 		int sum = a+b;
 		return sum;
 	}
-	public static void main(String[]args){
-		System.out.println(add(3,5));
-	}
+	
 }
